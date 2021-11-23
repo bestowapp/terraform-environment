08140101faea341f31c72236561d10f82d232374
Ryan Hanks <ryandhanks@gmail.com>
2021-11-23 16:11:46 -0600

    wip

--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
-#variable "billing_account" {}
-
-variable "name" {}
+variable "billing_account" {}
 
 variable "display_name" {}
 
-#variable "organization_id" {}
+variable "organization_id" {}
 
-variable "parent_folder_name" {}
+variable "bucket_name" {}
+
+variable "service_account_id" {}
 
 variable "service_account_email" {}
+
+variable "project_id" {}
+
+variable "name" {}
+
+variable "environment_group_name" {}
