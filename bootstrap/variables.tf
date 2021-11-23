5bb5b3f33c40b494c3b5ce1855fe1fc1abe3460e
Ryan Hanks <ryandhanks@gmail.com>
2021-11-23 17:32:40 -0600

    wip

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,7 +1,6 @@
 variable "billing_account" {}
 variable "bucket_name" {}
 variable "folder_id" {}
-variable "environment_name" {}
 variable "environment_group_name" {}
 variable "organization_id" {}
 variable "service_account_id" {}
