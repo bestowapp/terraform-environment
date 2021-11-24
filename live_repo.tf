a5349bef0a6f293f290eb11f5a71b51a8e52f716
Ryan Hanks <ryandhanks@gmail.com>
2021-11-23 19:24:09 -0600

    wip

--- live_repo.tf
+++ live_repo.tf
@@ -1,11 +1,11 @@
 locals {
-  live_repo_name = "live-${var.name}"
+  live_repo_name = "live-${var.environment_group_name}-${var.name}"
 }
 resource "github_repository" "live_environment_group" {
   name = local.live_repo_name
   visibility = "private"
   template {
     owner      = "bestowapp"
-    repository = "template-live-environment-group"
+    repository = "template-live-environment"
   }
 }
