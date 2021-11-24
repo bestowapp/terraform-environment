0c4025559543dd37fb5839acfba952185bbea8d1
Ryan Hanks <ryandhanks@gmail.com>
2021-11-23 19:00:20 -0600

    wip

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -6,18 +6,18 @@ resource "random_string" "project_id_suffix" {
   number  = false
 }
 
-#resource "google_storage_bucket" "state" {
-#  name                        = "${module.project.project_id}-state"
-#  location                    = "US"
-#  force_destroy               = true
-#  uniform_bucket_level_access = false
-#  project                     = module.project.project_id
-#  versioning {
-#    enabled = true
-#  }
-#}
-#resource "google_storage_bucket_iam_member" "member" {
-#  bucket = google_storage_bucket.state.name
-#  role   = "roles/storage.admin"
-#  member = "serviceAccount:${module.project.service_account_email}"
-#}
+resource "google_storage_bucket" "state" {
+  name                        = "${module.project.project_id}-state"
+  location                    = "US"
+  force_destroy               = true
+  uniform_bucket_level_access = false
+  project                     = module.project.project_id
+  versioning {
+    enabled = true
+  }
+}
+resource "google_storage_bucket_iam_member" "member" {
+  bucket = google_storage_bucket.state.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${module.project.service_account_email}"
+}
