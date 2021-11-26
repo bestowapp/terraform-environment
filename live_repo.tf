2ed82d07acf30923f7f25535cce4b75aea7136da
Ryan Hanks <ryandhanks@gmail.com>
2021-11-26 13:29:28 -0600

    wip

--- live_repo.tf
+++ live_repo.tf
@@ -1,7 +1,7 @@
 locals {
   live_repo_name = "live-${var.group_name}-${var.name}"
 }
-resource "github_repository" "live_environment_group" {
+resource "github_repository" "live_environment" {
   name = local.live_repo_name
   visibility = "private"
 #  template {
