b3786dafd59ca9c7378c3413d8b088d10f46f238
Ryan Hanks <ryandhanks@gmail.com>
2021-11-26 13:51:38 -0600

    wip

--- live_repo_secrets.tf
+++ live_repo_secrets.tf
@@ -8,16 +8,16 @@ locals {
 #    "project_id" : var.project_id,
     "environment_name" : var.name,
     "environment_group_name": var.group_name,
-#    "environment_folder_id" : google_folder.environment_folder.folder_id,
-#    "environment_folder_name" : google_folder.environment_folder.name,
+    "environment_folder_id" : google_folder.environment_folder.folder_id,
+    "environment_folder_name" : google_folder.environment_folder.name,
   })
 }
 
-#resource "github_actions_secret" "environment" {
-#  repository      = github_repository.live_environment.name
-#  for_each        = local.environment
-#  secret_name     = each.key
-#  plaintext_value = each.value
-#}
+resource "github_actions_secret" "environment" {
+  repository      = github_repository.live_environment.name
+  for_each        = local.environment
+  secret_name     = each.key
+  plaintext_value = each.value
+}
 
 
