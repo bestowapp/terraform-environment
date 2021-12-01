16b45f24cf016a218f2674bf646789cc64d73a24
Ryan Hanks <ryandhanks@gmail.com>
2021-11-30 19:12:37 -0600

    Add role bindings

--- project.tf
+++ project.tf
@@ -6,10 +6,10 @@ module "project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 11.2"
 
-  name              = local.project_name
-  org_id            = var.organization_id
-  billing_account   = var.billing_account
-  folder_id         = var.group_folder_id
+  name            = local.project_name
+  org_id          = var.organization_id
+  billing_account = var.billing_account
+  folder_id       = var.group_folder_id
 
   random_project_id = true
   activate_apis     = [
@@ -27,7 +27,11 @@ module "project" {
 
 resource "google_organization_iam_member" "admin_bindings" {
   member   = "serviceAccount:${module.project.service_account_email}"
-  org_id = var.organization_id
-  for_each = toset(["roles/billing.projectManager", "roles/billing.user", "roles/resourcemanager.projectCreator", "roles/iam.securityAdmin", "roles/resourcemanager.folderAdmin"])
-  role   = each.value
+  org_id   = var.organization_id
+  for_each = toset([
+    "roles/billing.projectManager", "roles/billing.user", "roles/resourcemanager.projectCreator",
+    "roles/resourcemanager.projectDeleter",
+    "roles/iam.securityAdmin", "roles/resourcemanager.folderAdmin", "roles/storage.admin"
+  ])
+  role     = each.value
 }
